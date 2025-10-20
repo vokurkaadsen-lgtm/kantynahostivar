ccb1f0a61d17e7e60a6bcc703ef5ee414787382a
VokyJ <jiri-vokurka@seznam.cz>
2025-10-20 11:27:58 +0200

    Initial commit from Create Next App

---
+++ src/app/chossecanteen/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChooseCanteenPage = () => {
+  return (
+    <div>ChooseCanteenPage</div>
+  )
+}
+
+export default ChooseCanteenPage
---
+++ src/lib/constants/features.ts
@@ -0,0 +1,14 @@
+const features = [
+    {
+        heading: "Přijatelná cena",
+        text: "Každé naše jídlo je plné chuti, která vám dodá energii a dobrou náladu, a přitom nezatíží váš rozpočet."
+    },
+    {
+        heading: Pohodlné a rychlé objednávání,
+        text: 
+    },
+    {
+        heading: Rozvoz po Hostivaři a jeho okolí,
+        text: 
+    }
+]
